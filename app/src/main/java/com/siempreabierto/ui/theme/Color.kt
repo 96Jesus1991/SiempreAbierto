f8749d983382272c3005d07cea42b6854db2807b
96Jesus1991 <jesuscoello961991@gmail.com>
2025-12-23 14:38:25 +0100

    Create Color.kt

---
+++ app/src/main/java/com/siempreabierto/ui/theme/Color.kt
@@ -0,0 +1,68 @@
+package com.siempreabierto.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Colores principales
+val Primary = Color(0xFF1E88E5)
+val PrimaryDark = Color(0xFF1565C0)
+val PrimaryLight = Color(0xFF42A5F5)
+
+// Acento - Naranja carretera
+val Accent = Color(0xFFFF6D00)
+val AccentDark = Color(0xFFE65100)
+val AccentLight = Color(0xFFFF9100)
+
+// Fondo oscuro (modo noche - por defecto)
+val BackgroundDark = Color(0xFF121212)
+val SurfaceDark = Color(0xFF1E1E1E)
+val CardDark = Color(0xFF2D2D2D)
+
+// Fondo claro (modo día)
+val BackgroundLight = Color(0xFFF5F5F5)
+val SurfaceLight = Color(0xFFFFFFFF)
+val CardLight = Color(0xFFFFFFFF)
+
+// Textos modo oscuro
+val TextPrimary = Color(0xFFFFFFFF)
+val TextSecondary = Color(0xB3FFFFFF)
+val TextHint = Color(0x66FFFFFF)
+
+// Textos modo claro
+val TextDark = Color(0xFF212121)
+val TextDarkSecondary = Color(0xFF757575)
+
+// Estados
+val Success = Color(0xFF4CAF50)
+val Warning = Color(0xFFFFC107)
+val Error = Color(0xFFF44336)
+val Info = Color(0xFF2196F3)
+
+// Emergencia
+val EmergencyRed = Color(0xFFFF1744)
+val EmergencyRedDark = Color(0xFFD50000)
+val EmergencyBackground = Color(0x33FF1744)
+
+// Categorías
+val CatWorkshop = Color(0xFF5C6BC0)
+val CatGasStation = Color(0xFF26A69A)
+val CatParking = Color(0xFF7E57C2)
+val CatFood = Color(0xFFEF5350)
+val CatRest = Color(0xFF42A5F5)
+val CatWash = Color(0xFF29B6F6)
+
+// Mapa
+val MapRoute = Color(0xFF1E88E5)
+val MapMarker = Color(0xFFFF6D00)
+val MapRestriction = Color(0xFFFF5252)
+val MapUserLocation = Color(0xFF4CAF50)
+
+// Vehículos
+val VehicleCar = Color(0xFF42A5F5)
+val VehicleTruck = Color(0xFFFF7043)
+val VehicleBus = Color(0xFF66BB6A)
+val VehicleCamper = Color(0xFFAB47BC)
+
+// Comunidad
+val CommunityVerified = Color(0xFF4CAF50)
+val CommunityPending = Color(0xFFFFC107)
+val CommunityHelper = Color(0xFF1E88E5)
